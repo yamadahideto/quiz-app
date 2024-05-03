6b7bc140c087fb70b0dd9ccb1062d615c6e17f5f
yamadahideto <yahideto428@gmail.com>
2024-05-03 17:28:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
+// App 関数のreturnの中身が画面に表示される
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [next, setNext] = useState(false);
+  const [answers, setAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [feedback, setFeedback] = useState(null);
+  const handleAnswer = (answer) => {
+    const newAnswer = {
+      question: quizData[currentQuestion].question,
+      answer: answer,
+      correct: answer === quizData[currentQuestion].correct,
+    };
+    console.log(newAnswer);
+
+    // 正解していた場合
+    if (newAnswer.correct) {
+      setScore((prevScore) => prevScore + 1);
+      setFeedback("⚪︎");
+    } else {
+      //不正解の場合
+      setFeedback("⚪︎");
+    }
+
+    setAnswers([...answers, newAnswer]);
+    // 初期の空の配列にnewAnswerの配列が代入される → [{},{},{}]
+    setNext(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz-container">
+      <div className="question-section">
+        <h1>
+          問題 {currentQuestion + 1} / {quizData.length}{" "}
+        </h1>
+        <h2> {quizData[currentQuestion].question} </h2>
+
+        <div className="answer-section">
+          {quizData[currentQuestion].options.map((item, index) => (
+            <button
+              key={index}
+              onClick={() => handleAnswer(item)}
+              className={`quiz-option-button option-${index}`}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
+
+const quizData = [
+  {
+    question: "太陽系で最も大きな惑星は？",
+    options: ["地球", "火星", "木製", "土星"],
+    correct: "木製",
+  },
+  {
+    question: "江戸川コナンの正体は？",
+    options: ["工藤新一", "服部平次", "怪盗キッド", "ひさじゅ"],
+    correct: "工藤新一",
+  },
+  {
+    question: "モナリザを書いたのは誰ですか？",
+    options: ["レオナルド", "火星", "木製", "土星"],
+    correct: "木製",
+  },
+  {
+    question: "太陽系で最も大きな惑星は？",
+    options: ["地球", "火星", "木製", "土星"],
+    correct: "木製",
+  },
+];
 
 export default App;
